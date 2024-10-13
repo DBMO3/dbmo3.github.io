486c49a5f6ccbd8ede8a23c9dfe9f47643984c6f
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2024-10-13 09:15:09 -0600

    Add files via upload
    Signed-off-by: Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function openWhatsApp() {
-    window.open("https://wa.me/527775693746");
+    window.open("https://wa.me/527775693746/?text=Hola%20DBMO3%20me%20interesa%20tu%20servicio", "_blank");
 }
